087c3f94e3447f75f96c3bb8f41824c3f94707f9
Mapou435 <eaa24mkrp@students.eaaa.dk>
2025-09-18 02:42:37 +0200

    Basic CSS
    Completed basic CSS to ensure overlay of the design was possible with karousel

--- src/components/Forside.jsx
+++ src/components/Forside.jsx
@@ -1,7 +1,10 @@
 export default function Forside() {
   return (
     <div className="forside">
-      <h1>This is test 3</h1>
+      <div className="forside-layout">
+        <img className="forside-image" src="/img/logo.png" alt="" />
+        <h1 className="forside-title">This is test 3</h1>
+      </div>
     </div>
   );
 }
